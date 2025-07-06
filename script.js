3603352c7d08a67298b2c2b426b54678252e2b98
utelbayiv-rasul <rasulutelbayivv@gmail.com>
2025-07-06 22:55:34 +0500

    Update script.js

--- script.js
+++ script.js
@@ -314,7 +314,7 @@ let songs = [
             { time: 1000, text: "I just wanna be yours (Wanna be yours)" }
         ]
     },
-    id: 7,
+    { id: 7,
         title: "Golden Brown",
         artist: "rasul",
         album: "AM",
